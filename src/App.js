9c6dad4231db6865f72c79c765cf2792ae64ea15
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-09-23 22:24:10 +0200

    added pages and a login button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Home from "./Pages/Home";
+import BlogId from "./Pages/BlogId";
+import Blogposts from "./Pages/Blogposts";
+import LoginPage from "./Pages/Login";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {NavLink} from "react-router-dom";
+
 
 function App() {
-  // We houden in de state bij of iemand is "ingelogd" (simpele versie)
-  const [isAuthenticated, toggleIsAuthenticated ] = useState(false);
+    // We houden in de state bij of iemand is "ingelogd" (simpele versie)
+    const [isAuthenticated, toggleIsAuthenticated] = useState(false);
 
-  return (
-    <div>
-      Maak hier jouw prachtige blog-applicatie!
-    </div>
-  );
+    return (
+        <Router>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/" exact activeClassname="active">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login" exact activeClassname="active">Login</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/blogposts" exact activeClassname="active">Blog</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login:id" exact activeClassname="active">Personal</NavLink>
+                    </li>
+                </ul>
+            </nav>
+
+
+            <Switch>
+                <Route exact path="/home/">
+                    <Home />
+                </Route>
+                <Route path="/blogposts:id">
+                    <BlogId />
+                </Route>
+                <Route path="/blogposts/">
+                    <Blogposts />
+                </Route>
+                <Route path="/login/">
+                    <LoginPage />
+                </Route>
+            </Switch>
+
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Pages/Login.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+function LoginPage() {
+    return (
+            <div className="loginButton">
+                <button type="button">Login</button>
+            </div>
+    )
+}
+
+
+export default LoginPage
